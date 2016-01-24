8db94d48bc3c54e227f4380d8265a479a641ccb8
Oleksii Fedorov <waterlink000@gmail.com>
2016-01-24 22:34:12 +0100

    Fix race condition in test setup

--- tests/lib.sh
+++ tests/lib.sh
@@ -1,12 +1,23 @@
 set -e
 set -x
 
+MAX_WAIT_RETRIES=20
 RECORDER_ENDPOINT=${RECORDER_ENDPOINT:-"http://localhost:9977"}
+
+wait_for_port() {
+  retries=0
+  while ! nc -z localhost $1 && [[ $retries -lt $MAX_WAIT_RETRIES ]]; do
+    sleep 0.1
+    retries=$((retries+1))
+  done
+  [[ $retries -lt $MAX_WAIT_RETRIES ]]
+}
 
 setup() {
   [[ -z "$NO_GOBUILD" ]] && go build
   ./recorder reset
   ./recorder daemon &
+  wait_for_port 9977
 }
 
 cleanup() {
