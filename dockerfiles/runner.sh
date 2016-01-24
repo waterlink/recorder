d5d7e5beedcdc33f8731738dac4d2d9b65b38da0
Oleksii Fedorov <waterlink000@gmail.com>
2016-01-24 23:20:14 +0100

    Add nice runner script for docker image

---
+++ dockerfiles/runner.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+RECORDER_PORT=${RECORDER_PORT:-9977}
+RECORDER_IMAGE=${RECORDER_IMAGE:-waterlink/recorder}
+
+need_user="-u $(id -u):$(id -g)"
+if [[ "$(uname)" = "Darwin" ]]; then
+  need_user=
+fi
+
+daemon_opts="-it --rm"
+if [[ "$1" = "daemon" ]]; then
+  daemon_opts="-d -p ${RECORDER_PORT}:9977"
+fi
+
+dir_opts="-v /tmp/.recorder:/tmp/.recorder -w /tmp"
+
+docker run \
+  $need_user \
+  $dir_opts \
+  $daemon_opts \
+  $RECORDER_IMAGE \
+  recorder "$@"
